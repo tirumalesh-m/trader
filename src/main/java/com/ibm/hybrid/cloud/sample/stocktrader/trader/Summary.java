52efdc61b899fb508b44e575ded3704ea3aad06b
tirumalesh-m <36234463+tirumalesh-m@users.noreply.github.com>
2025-11-24 16:38:43 +0530

    Update Summary.java
    changed the servletsecurity to 
    @ServletSecurity(@HttpConstraint(rolesAllowed = {}))

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/Summary.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/trader/Summary.java
@@ -60,7 +60,8 @@ import org.apache.commons.math3.stat.descriptive.SynchronizedDescriptiveStatisti
  * Servlet implementation class Summary
  */
 @WebServlet(description = "Broker summary servlet", urlPatterns = { "/summary" })
-@ServletSecurity(@HttpConstraint(rolesAllowed = { "StockTrader", "StockViewer" } ))
+//@ServletSecurity(@HttpConstraint(rolesAllowed = { "StockTrader", "StockViewer" } ))
+@ServletSecurity(@HttpConstraint(rolesAllowed = {}))
 @ApplicationScoped
 public class Summary extends HttpServlet {
 	private static final long serialVersionUID = 4815162342L;
